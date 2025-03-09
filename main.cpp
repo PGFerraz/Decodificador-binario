97b855e21c77d32514f3a331377481f7f8dd51aa
Pedro Gabriel Ferraz Santos Silva <144126732+PGFerraz@users.noreply.github.com>
2025-03-09 14:51:51 -0300

    Conversão para ASCII

--- main.cpp
+++ main.cpp
@@ -2,61 +2,61 @@
 #include <stack>
 #include <string>
 #include <fstream>
-/*
-Fazer um código em C++ capaz de lê um arquivo contendo apenas 0's e 1's. O programa deverá ler esse arquivo e a cada 8 bits deverá converter 
-o código binário em decimal e de decimal para o código ASCII correspondente. Exemplo:
-conteúdo do arquivo: 010000010110110001101111
-separando de oito em oito bits teremos:
-01000001 = A
-01101100 = l
-01101111 = o
 
-formando então a palavra "Alo".
-
-é obrigatório a utilização de pelo menos uma pilha no código.
-*/
 using namespace std;
 
-void converter_para_decimal(stack <string>& a){
-    int decimal = 0;
+void converter_para_decimal(stack<int>& a) 
+{
     int potencia = 1;
-    while(!a.empty()){  // Enquanto a pilha não estiver vazia
-        for(int i=0;i<7;i++){
-            decimal += a.top() * potencia;// Pega o topo da pilha (um número inteiro)
-            potencia *= 2; // multiplica por 2^0, 2^1,...
+    int n = 0;
+    int matriz_dec[100];
+
+    while (!a.empty()) {
+        int decimal = 0;
+        for (int i = 0; i < 8; i++) {
+            decimal += a.top() * potencia;
+            potencia *= 2;
             a.pop();
         }
-        cout << "\nValor decimal: " << decimal;
+
+        matriz_dec[n] = decimal;
+        cout << "Decimal: " << matriz_dec[n] << " | Caracter ASCII: " << char(matriz_dec[n]) << "\n";
+        n++;
+        potencia = 1;
     }
 }
 
-int main()
-{
+int main() {
     string nome;
     ifstream ler;
-    stack <string> binario_invertido, binario_correto, copia;
-    string numeros;
-    
+    stack<int> binario_invertido, binario_correto;
+    char numeros;
+
     cout << "Digite o nome do arquivo: ";
     getline(cin, nome);
     
-    ler.open(nome); ;// abre arquivo para gravação
-    if(ler.is_open()){
-        while(ler >> numeros){  // lê os números até o final do arquivo
-            binario_invertido.push(numeros); // adiciona os números a uma pilha
+    ler.open(nome);
+    if (ler.is_open()) {
+
+        while (ler >> numeros) {
+            if(numeros == '0') {
+                binario_invertido.push(0);
+            } else if(numeros == '1') {
+                binario_invertido.push(1);
+            }
         }
         ler.close();
-        while(!binario_invertido.empty()){
-            binario_correto.push(binario_invertido.top());
-            binario_invertido.pop();
+
+        if (binario_invertido.size() % 8 != 0) {
+            cerr << "Erro: o número de bits no arquivo não é um múltiplo de 8!" << endl;
+            return 1;
         }
-        converter_para_decimal(binario_correto);
-    }else {
-        cerr << "Erro ao abrir o arquivo";
+
+        converter_para_decimal(binario_invertido);
+    } else {
+        cerr << "Erro ao abrir o arquivo" << endl;
+        return 1;
     }
-    
-    
-    cout<<"Hello World";
 
     return 0;
 }
